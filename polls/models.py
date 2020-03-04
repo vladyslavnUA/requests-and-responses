e79002de181a3f3bdc68edc63060215e0958a778
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-04 02:29:04 -0800

    updated models

--- polls/models.py
+++ polls/models.py
@@ -6,16 +6,16 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
     
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now 
-        was_published_recently.admin_order_field = 'pub_date'
-        was_published_recently.boolean = True
-        was_published_recently.short_description = 'Published recently?'
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -25,14 +25,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-# eventbride:
-
-# class Event(models.Model):
-#     title = models.CharField(max_length=128)
-#     members = models.ManyToManyField(Person, through='Membership')
-
-
-# class Attendee(models.Model):
-#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    
+        
