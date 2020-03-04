0f1a5a91a7756f080bf9165ff9e9f6620e9d25c7
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-04 02:37:27 -0800

    added api app

---
+++ api/views.py
@@ -0,0 +1,33 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from polls.models import Question
+from api.serializers import QuestionSerializer
+from api.serializers import ChoiceSerializer
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+class QuestionList(ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(RetrieveDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    
+    def vote(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+            return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
