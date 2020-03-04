574096479216fb0a734c99b76ba45921b7562256
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-04 02:28:12 -0800

    udpated admin

--- polls/admin.py
+++ polls/admin.py
@@ -4,21 +4,22 @@ from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-
+    admin.site.register(Question)
+    admin.site.register(Choice)
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+    # model = Choice
+    # extra = 3
 
-admin.site.register(Question, QuestionAdmin)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ChoiceInline]
+
+# admin.site.register(Question, QuestionAdmin)
 
 #todo: add bootstrap, deploy to heroku
